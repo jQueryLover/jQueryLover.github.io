10992dba47eee91227ba59128a3b29e597d8f713
jQueryLover <47346667+jQueryLover@users.noreply.github.com>
2019-02-09 15:28:11 +1100

    Updates the website, adding comments
    Useful if someone comes across this code.

--- clicker.js
+++ clicker.js
@@ -1,7 +1,9 @@
+// This creates the main header.
 var jQueryClickerHeader = $("<h1>")
   .text("jQuery Clicker Game")
   .appendTo("body")
 
+// This creates the jQuery logo and sets it up for the game.
 var jQueryClickerImage = $("<img>")
   .attr("src", "jquery-logo.png")
   .attr("width", "300")
@@ -9,23 +11,34 @@ var jQueryClickerImage = $("<img>")
   .css("cursor", "pointer")
   .appendTo("body")
 
+// This creates a place for the click counter.
 var jQueryDiv = $("<div>")
   .attr("id", "results")
 
+// This is the click counter. It is edited using jQuery, of course.
 var jQueryClickerResult = $("<p>")
   .attr("id", "results")
   .appendTo("body")
 
-var numClicks = 0;
+// This resets the number of clicks to 0.
+var numClicks = 0
+
+// This part edits the click counter.
 $("#results").text("You clicked " + numClicks + " times");
 
+// To be honest, I have no idea why this is here. I just copy pasted some old code that I made,
 $("#")
 
+// This is the function that makes jQuery do something when you click.
 $("img").on("click", function() {
-  numClicks += 1;
-  $("#results").text("You clicked " + numClicks + " times");
+
+  numClicks += 1; // This changes the number of clicks by one every time you click.
+
+  $("#results").text("You clicked " + numClicks + " times"); // This part edits the click counter. I have no idea why it's repeated, but
+                                                             // I'm not taking any chances by deleting it.
 });
 
+// This part is all the CSS styles from the main page, without the style for the link.
 $("body").css("background-color", "rgb(66, 25, 106)")
 $("body").css("color", "white")
 $("body").css("font-family", "sans-serif")
--- jquery.js
+++ jquery.js
@@ -1,46 +1,65 @@
+// This creates a jQuery logo and adds all the required attributes.
 var jQueryLogo = $("<img>")
   .attr("src", "jquery-logo.png")
   .attr("width", "150")
   .attr("alt", "jQuery logo")
   .appendTo("body")
 
+// This creates the main header.
 var mainHeader = $("<h1>")
   .text("Hello!")
   .appendTo("body")
 
+// This creates the paragraph after the main header.
 var mainHeaderInfo = $("<p>")
   .html("This is a just a little test of a website that is fully ran by jQuery. When I say I like jQuery, I <em> really </em> like jQuery. <br> NOTE: All links on this page are white with underlines so don't expect any normal looking links.")
   .appendTo("body")
 
+// This creates a line break. I can't find a efficent way to do this though.
+// If you do know a way, please consider contributing at my GitHub page!
 var lineSpace = $("<p>")
   .html("<br> <br>")
   .appendTo("body")
 
+// This creates a paragraph with lots of useful links.
 var pageLinks = $("<p>")
   .html("Here are some handy dandy page links: <br>")
   .appendTo("body")
 
+// This is a link to my little clicker game. I don't have any plans to improve it.
 var clickerPageLink = $("<a>")
   .text("Clicker game thing")
   .attr("href", "clicker.html")
   .appendTo("body")
 
+// This creates a line break. I can't find a efficent way to do this though.
+// If you do know a way, please consider contributing at my GitHub page!
 var lineSpace = $("<p>")
   .html("<br> <br>")
   .appendTo("body")
 
+// This creates the second header in the page.
 var aboutjQuery = $("<h2>")
   .text("But what is jQuery?")
   .appendTo("body")
 
+// This creates a paragraph after the second header.
 var aboutjQueryInfo = $("<p>")
   .html("jQuery is a JavaScript library that makes JavaScript better. For more infomation, visit <a href = https://google.com> this handy dandy Wikipedia page. </a>")
   .appendTo("body")
 
+// This creates a line break. I can't find a efficent way to do this though.
+// If you do know a way, please consider contributing at my GitHub page!
 var lineSpace = $("<p>")
   .html("<br> <br>")
   .appendTo("body")
 
+// This creates a header with a part so I can explain a bit more about me.
+var aboutMeHeader = $("<h2>")
+  .text("Wait, who are you?")
+  .appendTo("body")
+
+// This part are all the CSS styles used.
 $("body").css("background-color", "rgb(66, 25, 106)")
 $("body").css("color", "white")
 $("body").css("font-family", "sans-serif")
