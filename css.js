3c8b58600e73ab526473f0b83a44345a2015bb24
jQueryLover <47346667+jQueryLover@users.noreply.github.com>
2019-03-08 19:58:22 +1100

    Updates css.js to add a custom font
    Also makes the page a bit bigger. Credits for the Google font function go to Archer on Stack Overflow!

--- css.js
+++ css.js
@@ -1,11 +1,25 @@
+// This loads a custom Google font. Thanks to Archer from Stack Overflow!
+function addGoogleFont(FontName) {
+    $("head").append("<link href='https://fonts.googleapis.com/css?family=" + FontName + "' rel='stylesheet' type='text/css'>");
+}
+
+addGoogleFont("Special+Elite");
+addGoogleFont("Oswald");
+
 // This sets the background colour to a nice purple colour.
 $("body").css("background-color", "rgb(50, 22, 96)")
 
 // This sets the text colour to white.
 $("body").css("color", "white")
 
-// This sets the font to the default sans serif installed on the system. I'm hoping to get a custom font up and running soon.
-$("body").css("font-family", "sans-serif")
+// This sets the heading font to the cool font named Special Elite.
+$("h1, h2, h3, h4, h5, h6").css("font-family", "'Special Elite', 'serif'")
+
+// This sets the paragraph font to the font names Oswald.
+$("p, a").css("font-family", "'Oswald', 'sans-serif'")
+
+// This makes the page font a bit bigger so we can fully appreciate these fonts.
+$("body").css("font-size", "19px")
 
 // This changes the link colour to white to match the text colour.
 $("a").css("color", "rgb(255, 255, 255)")
