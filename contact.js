b18140a956ce6555db4a55f30ceffffea574f45a
jQueryLover <47346667+jQueryLover@users.noreply.github.com>
2019-03-11 17:02:27 +1100

    Update contact.js

--- contact.js
+++ contact.js
@@ -52,12 +52,6 @@ var discordServerInfo = $("<p>")
   .html("Here are a collection of server links that you may join. <br>")
   .appendTo("body")
 
-var computerClan = $("<a>")
-  .attr("href", "https://discordapp/invite/nTazaMT")
-  .html("My new Discord server called Computer Clan.")
-  .appendTo("body")
-
-var tPoseClub = $("<a>")
-  .attr("href", "https://discordapp/invite/bZE897Q")
-  .html("My friend's Discord server called T-Pose Club, mainly about the Fortnite emote T-Pose. <br>")
+var comingSoon = $("<p>")
+  .text("Links here coming soon!"
   .appendTo("body")
