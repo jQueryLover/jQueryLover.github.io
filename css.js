9fdaa08c805ffb4a71092d048e94e2c9f7d47d94
jQueryLover <47346667+jQueryLover@users.noreply.github.com>
2019-03-13 19:55:39 +1100

    Update css.js comments

--- css.js
+++ css.js
@@ -9,16 +9,7 @@ addGoogleFont("Special+Elite");
 // This adds the body font.
 addGoogleFont("Oswald");
 
-/*
-var favicon = $("<link>")
-  .attr("href, favicon.ico")
-  .attr("rel", "icon")
-  .attr("type", "image/x-icon")
-  .appendTo("head")
-<link href="/YOUR_PATH/favicon.ico" rel="icon" type="image/x-icon" />
-*/
-
-// This sets the background colour to a nice purple colour.
+// This sets the background colour to a nice dark green colour.
 $("body").css("background-color", "rgb(21, 124, 31)")
 
 // This sets the text colour to white.
@@ -27,7 +18,7 @@ $("body").css("color", "white")
 // This sets the heading font to the cool font named Special Elite.
 $("h1, h2, h3, h4, h5, h6").css("font-family", "'Special Elite', 'serif'")
 
-// This sets the paragraph font to the font names Oswald.
+// This sets the paragraph font to the font named Oswald.
 $("p, a").css("font-family", "'Oswald', 'sans-serif'")
 
 // This makes the page font a bit bigger so we can fully appreciate these fonts.
